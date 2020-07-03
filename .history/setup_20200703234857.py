e4302692aa80746996f3fb032403fe2d40e740cf
Weifeng Jiang <jiangweifeng@live.com>
2020-07-03 23:50:41 +0800

    change name to pdynamics

--- .history/setup_20200703234857.py
+++ .history/setup_20200703234857.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pdynamics-jiangweifeng",
+    name="dynamics365-jiangweifeng", # Replace with your own username
     version="0.1.3",
     author="Weifeng Jiang",
     author_email="jiangweifeng@live.com",
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3',
+    python_requires='>=3.5',
 )
